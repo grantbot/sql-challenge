2dc998b752bc4e325f9e865d87adda274ac38616
Grant <grantbot@gmail.com>
2021-05-18 10:55:44 -0400

    tables.sql completed, query work started

---
+++ EmployeeSQL/query.sql
@@ -0,0 +1,59 @@
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+select employees.emp_no,
+employees.last_name,
+employees.first_name,
+employees.sex,
+salaries.salary
+from employees, salaries
+where employees.emp_no = salaries.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+-- select employees.last_name,
+-- employees.first_name,
+-- employees.hire_date,
+-- date_part(‘year’,TIMESTAMP ‘2017-01-01’)
+-- from employees;
+SELECT *
+from employees;
+
+
+
+
+
+
+
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+
+
+
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+
+
+
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+
+
+
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+
+
+
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+
+
+
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
---
+++ EmployeeSQL/tables.sql
@@ -0,0 +1,54 @@
+-- creating titles set title_id as primary key
+create table titles (
+	title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
+	primary key (title_id)
+);
+
+-- creating employees table with primary key and foreign key referencing title_id
+
+create table employees (
+	emp_no INT NOT NULL,
+	emp_title_id VARCHAR,
+	birth_date VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR,
+	hire_date VARCHAR NOT NULL,
+	primary key (emp_no),
+	foreign key (emp_title_id) references titles (title_id)
+);
+
+Create Table salaries (
+	emp_no INT NOT NULL,
+	salary INT NOT NULL,
+	primary key (emp_no),
+	foreign key (emp_no) references employees (emp_no)
+);
+
+create table departments (
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
+	primary key (dept_no)
+);
+
+create table dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	primary key (emp_no, dept_no),
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no)
+);
+
+create table dept_manager (
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+	primary key (dept_no, emp_no),
+	foreign key (emp_no) references employees (emp_no),
+	foreign key (dept_no) references departments (dept_no)
+);
+
+
+
+
+
